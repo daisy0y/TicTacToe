e2cde2029e1854e347a85c3a5cbb45d78f114be9
daisy0y <61371367+daisy0y@users.noreply.github.com>
2020-08-31 23:12:48 +0900

    200831.1
    1. 시간기능 추가중.
    2. 시간 초과시 정상 작동
    3. 보드판 조건 달성시 종료얼럿 동작 후 타미어 재시작되는 현상있음

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-//how to play 사용설명 
 //타이머기능넣기
 // 무승부 오류는 drawValue 값을 클릭할때 주는거에서 O,X 입력될때마다 value값을 1씩 더하게 변경하여 오류 수정
+const GAME_TIME = 5;
 const td = document.querySelectorAll("td");
-let turn = true;  // 순서 초기값
 const startBtn = document.getElementById("start");
 const tableMain = document.getElementById("tableMain");
 const reset = document.getElementById("reset");
+const timeDisplay = document.querySelector("#time")
+
+let turn = true;  // 순서 초기값
 let drawValue = 1; // 무승부 초기값
+let setTime = GAME_TIME;
+let countDown;
 
 function pushO(tdTarget){
     tdTarget.innerHTML="O";
@@ -21,19 +25,23 @@ function pushX(tdTarget){
 }
 
 
-// 차례알림 추가하기
+// 알림 추가하기
 function playing(e){
     if(turn && e.target.innerHTML.length === 0) {
+        resetInterval();
         pushO(e.target);
         turnAlert();
         gameEnd();
         draw();
+        run();
         }
     else if(!turn && e.target.innerHTML.length=== 0){
+        resetInterval();
         pushX(e.target);
         turnAlert();
         gameEnd();
         draw();
+        run();
        }
 }
 
@@ -42,7 +50,7 @@ function clickTd(){
     for(let i = 0 ; i < td.length; i++){
       td[i].addEventListener("click",function(e){
         playing(e);
-        
+       
         });
        }
     }
@@ -52,7 +60,8 @@ clickTd();
 function start(){   
         startBtn.addEventListener("click",function(){
         tableMain.style.pointerEvents="auto"
-        setInterval(countDown,1000);
+        resetInterval();
+        run();
 
     })
 }
@@ -60,11 +69,13 @@ start();
 
 // 리셋버튼
 function clickReset(){
+    
     reset.addEventListener("click",function(){
         for(let i = 0 ; i < td.length;i++){
             td[i].innerText="";
             tableMain.style.pointerEvents="none"
             drawValue = 0 ;
+            resetInterval();
        }
         }
        )}
@@ -76,11 +87,12 @@ function gameReset(){
     for(let i = 0 ; i < td.length;i++){
         td[i].innerText="";
         tableMain.style.pointerEvents="none"
+        
     }
 }
 
 
-// 차례알림
+// 순서알림
 function turnAlert(){
     if(turn === true){
         document.getElementById("turn").innerHTML="O";
@@ -100,10 +112,13 @@ function gameEnd(){
        || td[1].innerText == "O" && td[4].innerText == "O" && td[7].innerText == "O"
        || td[2].innerText == "O" && td[5].innerText == "O" && td[8].innerText == "O"
        || td[0].innerText == "O" && td[4].innerText == "O" && td[8].innerText == "O"
-       || td[2].innerText == "O" && td[4].innerText == "O" && td[6].innerText == "O"){
-      alert("O 팀이 승리하였습니다 ! 축하합니다!");
-      gameReset();
-      drawValue = 0;
+       || td[2].innerText == "O" && td[4].innerText == "O" && td[6].innerText == "O"
+       || time === 0 && turn === false
+       ){
+           resetInterval();
+           gameReset();
+           drawValue = 0;
+           alert("O 팀이 승리하였습니다 ! 축하합니다!");
    }else if(td[0].innerText == "X" && td[1].innerText == "X" && td[2].innerText == "X"
    || td[3].innerText == "X" && td[4].innerText == "X" && td[5].innerText == "X"
    || td[6].innerText == "X" && td[7].innerText == "X" && td[8].innerText == "X"
@@ -111,34 +126,70 @@ function gameEnd(){
    || td[1].innerText == "X" && td[4].innerText == "X" && td[7].innerText == "X"
    || td[2].innerText == "X" && td[5].innerText == "X" && td[8].innerText == "X"
    || td[0].innerText == "X" && td[4].innerText == "X" && td[8].innerText == "X"
-   || td[2].innerText == "X" && td[4].innerText == "X" && td[6].innerText == "X"){
-  alert("X 팀이 승리하였습니다 ! 축하합니다!");
-  gameReset();
-  drawValue = 0;
+   || td[2].innerText == "X" && td[4].innerText == "X" && td[6].innerText == "X"
+   || time === 0 && turn === true
+   ){
+       resetInterval();
+       gameReset();
+       drawValue = 0;
+       alert("X 팀이 승리하였습니다 ! 축하합니다!");
 }}
 gameEnd();
 
 // 무승부 함수
 function draw(){
     if(drawValue === 9){
-        alert("무승부 입니다!");
+        resetInterval();
         drawValue = 0;
         gameReset();
+        alert("무승부 입니다!");
     }
     }
     
 // 타이머
+// 시작을 누르면 카운트다운 시작
+// 시간내에 마무리 하지 못하면 현재순서 패배 처리
+// 칸 누를때마다 리셋되어 재시작되게
+// 게임 끝났을때마다 카운트리셋
 
-let time = 5;
-const timeDisplay = document.querySelector("#time")
+function run(){
+    resetInterval();
+    setTime = GAME_TIME;
+    countDown = setInterval(countInterval,1000);
 
+}
 
-
-function countDown(){
- if(time >0){
-    time--
-    timeDisplay.innerText=time
-
-
+function countInterval(){
+     if( setTime > 0 )
+ {
+    setTime--
+    timeDisplay.innerText = setTime;
   }
+   else if(setTime === 0 && turn === true){ 
+    alert("X 팀이 승리하였습니다 ! 축하합니다!");
+    gameReset();
+    drawValue = 0;
+    setTime = GAME_TIME;
+    timeDisplay.innerText = setTime;
+      clearInterval(countDown)
+  }else if(setTime === 0 && turn === false){ 
+    alert("O 팀이 승리하였습니다 ! 축하합니다!");
+    gameReset();
+    drawValue = 0;
+    setTime = GAME_TIME;
+    timeDisplay.innerText = setTime;
+      clearInterval(countDown)
+  }else{
+    setTime = GAME_TIME;
+    timeDisplay.innerText = setTime;
+      clearInterval(countDown)
+  }
+
+}
+
+function resetInterval(){
+    setTime = GAME_TIME;
+    timeDisplay.innerText = setTime;
+    clearInterval(countDown)
+
 }
